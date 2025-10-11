2c405e9a48d921f488103e3aecd3f106bd9b5220
GitHub Action <action@github.com>
2025-10-11 11:36:46 +0000

    Auto-update image list [skip ci]

--- image-list.js
+++ image-list.js
@@ -112,4 +112,5 @@ const imageList = [
 "pixelart-cupcake-2025-10-11T11-31-53.png",
 "pixelart-cupcake-2025-10-11T11-33-41.png",
 "pixelart-cupcake-2025-10-11T11-35-12.png",
+"pixelart-cupcake-2025-10-11T11-36-33.png",
 ];
